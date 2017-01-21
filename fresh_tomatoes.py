3f1007caa00d8cf44062d131931dc858e7b74b58
Alex <grievea86@gmail.com>
2017-01-21 16:12:03 -0600

    Refactored Movie class as a parent of Media, added TV class.

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -128,7 +128,7 @@ movie_tile_content = '''
 </div>
 '''
 
-
+# TO DO change this from movie specific to media specific
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
@@ -149,7 +149,7 @@ def create_movie_tiles_content(movies):
         )
     return content
 
-
+# TO DO change this from movie specific to media specific
 def open_movies_page(movies):
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
--- media.py
+++ media.py
@@ -1,17 +1,31 @@
 import webbrowser
 
-# The Movie Class
-class Movie():
-	""" This class provides a way to store movie related information"""
-
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-	# self is the instance of the class
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
+# Parent class to the Movie and TV Class, stores basic media information
+class Media():
+	def __init__(self, title, description, poster_image, trailer_youtube):
+		self.title = title
+		self.storyline = description
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
+
+
+# The Movie Class, child of the Media Class
+class Movie(Media):
+	"""This class provides a way to store movie specific information"""
+
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+	# self is the instance of the class
+	def __init__(self, title, storyline, poster_image, trailer_youtube):
+		Media.__init__(self, title, storyline, poster_image, trailer_youtube)
+
+
+# The Movie Class, child of the Media Class
+class TVShow(Media):
+	"""This class provides a way to store TV specific information"""
+	def __init__(self, title, storyline, poster_image, trailer_youtube):
+		Media.__init__(self, title, storyline, poster_image, trailer_youtube)
+
 
