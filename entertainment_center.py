61e9a20c4e621ef17abe95297023851efbff5d99
Alex <grievea86@gmail.com>
2017-01-22 10:09:32 -0600

    Removed old html output file, fixed Mad Men trailer.

--- entertainment_center.py
+++ entertainment_center.py
@@ -11,7 +11,7 @@ oceans_11 = media.Movie("Oceans Eleven", "Pretty people steal money", "https://u
 # Add TV Shows to the website
 arrested_development = media.TVshow("Arrested Development", "The story of a wealthy family who lost everything and the one son who had no choice but to keep them all together", "https://upload.wikimedia.org/wikipedia/commons/e/e3/Arrested_Development.svg", "https://www.youtube.com/watch?v=TntmMY7N8ag","Netflix")
 breaking_bad = media.TVshow("Breaking Bad", "A high school chemistry teach decides to cook meth", "https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png" ,"https://www.youtube.com/watch?v=HhesaQXLuRY", "AMC")
-mad_men = media.TVshow("Mad Men", "An ad man from the 60s drinks a lot","https://upload.wikimedia.org/wikipedia/en/3/33/Mad-men-title-card.jpg","https://www.youtube.com/watch?v=LfuMhXcLa-Q","AMC")
+mad_men = media.TVshow("Mad Men", "An ad man from the 60s drinks a lot","https://upload.wikimedia.org/wikipedia/en/3/33/Mad-men-title-card.jpg","https://www.youtube.com/watch?v=m7NChV93LBw","AMC")
 
 
 movies = [toy_story, avatar, matrix, oceans_11]
